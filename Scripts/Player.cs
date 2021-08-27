6f39346c0ac9277770961ad366eeda481a544be1
BarkhatovAV <barkhatov.a.v@mail.ru>
2021-08-27 14:48:12 +0300

    v1.2

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,106 +1,3 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.Events;
-
-//[RequireComponent(typeof(Animator))]
-//public class Player : MonoBehaviour
-//{
-//    [SerializeField] private int _health;
-//    [SerializeField] private List<Weapon> _weapons;
-//    [SerializeField] private Transform _shootPosition;
-
-//    private Weapon _currentWeapon;
-//    private int _currentWeaponNumber = 0;
-//    private int _currentHealth;
-//    private Animator _animator;
-
-//    public int Money { get; private set; }
-
-//    public event UnityAction<int, int> HealthChanged;
-//    public event UnityAction<int> MoneyChanged;
-
-//    private void Start()
-//    {
-//        //_currentWeapon = _weapons[_currentWeaponNumber];
-//        ChangeWeapon(_weapons[_currentWeaponNumber], _currentWeapon);
-//        _currentHealth = _health;
-//        _animator = GetComponent<Animator>();
-
-//    }
-
-//    private void Update()
-//    {
-//        if (Input.GetMouseButtonDown(0))
-//        {
-//            _animator.Play("Shoot");
-//            _currentWeapon.Shoot(_shootPosition);
-//        }
-//    }
-
-//    public void ApplyDamage(int damage)
-//    {
-//        _currentHealth -= damage;
-//        HealthChanged?.Invoke(_currentHealth, _health);
-//        if (_currentHealth <= 0)
-//        {
-//            Destroy(gameObject);
-//        }
-
-//    }
-
-//    public void AddMoney(int money)
-//    {
-//        Money += money;
-//        MoneyChanged?.Invoke(Money);
-//    }
-
-//    public void BuyWeapon(Weapon weapon)
-//    {
-//        Money -= weapon.Price;
-//        MoneyChanged?.Invoke(Money);
-//        var purchasedWeapon = Instantiate(weapon, this.transform);//
-//        purchasedWeapon.gameObject.SetActive(true);//
-//        _weapons.Add(purchasedWeapon);//
-//    }
-
-//    public void NextWeapon()
-//    {
-//        Weapon previuosWeapon = _currentWeapon;
-//        if (_currentWeaponNumber == _weapons.Count - 1)
-//        {
-//            _currentWeaponNumber = 0;
-//        }
-//        else
-//        {
-//            _currentWeaponNumber++;
-//        }
-
-//        ChangeWeapon(_weapons[_currentWeaponNumber], previuosWeapon);
-//    }
-
-//    public void PreviousWeapon()
-//    {
-//        Weapon previuosWeapon = _currentWeapon;
-//        if (_currentWeaponNumber == 0)
-//        {
-//            _currentWeaponNumber = _weapons.Count - 1;
-//        }
-//        else
-//        {
-//            _currentWeaponNumber--;
-//        }
-
-//        ChangeWeapon(_weapons[_currentWeaponNumber], previuosWeapon);
-//    }
-
-//    private void ChangeWeapon(Weapon currentWeapon, Weapon previousWeapon)
-//    {
-//        _currentWeapon = currentWeapon;
-//    }
-//}
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -134,7 +31,7 @@ public class Player : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            _animator.Play("Shoot");//
+            _animator.Play("Shoot");
             _currentWeapon.Shoot(_shootPoint);
         }
     }
@@ -160,8 +57,6 @@ public class Player : MonoBehaviour
     {
         Money -= weapon.Price;
         MoneyChanged?.Invoke(Money);
-        //Instantiate(weapon, this.transform);
-        //_weapons.Add(weapon);
         var purchasedWeapon = Instantiate(weapon, this.transform);
         _weapons.Add(purchasedWeapon);
     }
@@ -190,6 +85,4 @@ public class Player : MonoBehaviour
     {
         _currentWeapon = currentWeapon;
     }
-
-
 }
